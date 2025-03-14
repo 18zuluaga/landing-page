f96b177d3c0ca4b4ba6ca048879ef5d7c17c3fcc
18zuluaga <18zuluaga1989@gmail.com>
2025-03-14 16:30:12 -0500

    add value for default

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -9,13 +9,26 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
   formulario: FormGroup
+  userActive = {
+    name: 'Fernando',
+    apellido: 'Gomez'
+  }
+
+
+  ngOnInit(): void {
+    // this.formulario.get('name')?.setValue(this.userName) forma para solo un campo
+    this.formulario.patchValue({name: this.userActive.name, apellido: this.userActive.apellido})
+    this.formulario.get('name')?.disable()
+    this.formulario.get('apellido')?.disable()
+  }
 
   constructor( private form: FormBuilder) {
     this.formulario = this.form.group({
       name: ['', Validators.required],
+      apellido: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]]
     })
   }
